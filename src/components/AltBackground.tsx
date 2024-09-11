5f33b2c367fe81667b43ecf7819d9e273c5b2ab4
Christian Elijah Alcantara <chrsalctra@icloud.com>
2024-09-11 02:21:22 -0400

    gray

--- src/components/AltBackground.tsx
+++ src/components/AltBackground.tsx
@@ -3,7 +3,7 @@
 const AltBackground = () => {
   //Noise & Texture plugin developed byhttps://x.com/intent/follow?screen_name=rogie
   return (
-    <div className="invert fixed inset-0 w-full h-full overflow-hidden z-[-1]">
+    <div className="invert grayscale fixed inset-0 w-full h-full overflow-hidden z-[-1]">
       <iframe
         width="110%"
         height="100%"
