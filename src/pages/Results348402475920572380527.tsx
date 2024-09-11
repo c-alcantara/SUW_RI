da08e245c4c2152092cc74c88f25a7e12d8a9bc4
Christian Elijah Alcantara <chrsalctra@icloud.com>
2024-09-11 02:24:37 -0400

    unique

--- src/pages/Results348402475920572380527.tsx
+++ src/pages/Results348402475920572380527.tsx
@@ -54,7 +54,7 @@ export default function Results348402475920572380527() {
     <div className="container mx-auto py-10 h-screen flex items-center justify-center relative">
       {showBckg && <div className="fade-out"></div>}
       <div className="relative z-10 bg-gradient-to-t from-[rgba(255,255,255,1)] to-[rgba(255,255,255,.7)] rounded-2xl shadow-lg p-6 max-w-md">
-        <h2 className="text-2xl font-bold mb-4">Results Report (Ascending)</h2>
+        <h2 className="text-2xl font-bold mb-4">SUWRI Results</h2>
         <table className="min-w-full bg-transparent ">
           <thead>
             <tr>
