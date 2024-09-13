789a90a8280bce0830ab550c57b770c48aa75619
Christian Alcantara <chrsalctra@icloud.com>
2024-09-13 13:35:41 -0400

    zoom fix mobile

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -4,10 +4,7 @@ import Head from 'next/head';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* Other head elements can go here */}
-      </Head>
+
       <body className="bg-background text-foreground">
         {children}
       </body>
